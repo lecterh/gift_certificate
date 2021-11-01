bdf029a7b8c25b943f5d5685be27471cc738f58b
Aleksey <aleksey.narkevich@gmail.com>
2021-11-01 14:12:02 +0300

    add ru_locale file for exception

--- service/src/main/java/com/epam/esm/exception/util/ExceptionMessageReader.java
+++ service/src/main/java/com/epam/esm/exception/util/ExceptionMessageReader.java
@@ -1,15 +1,18 @@
 package com.epam.esm.exception.util;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ResourceBundle;
 
 @Component
+@AllArgsConstructor
 public class ExceptionMessageReader {
 
-    private final ResourceBundle bundle = ResourceBundle.getBundle("messages");
+    private final ResourceBundle bundle = ResourceBundle.getBundle("errorMessage");
 
     public String readMessage(String codeMessage) {
+
         return bundle.getString(codeMessage);
     }
 }
